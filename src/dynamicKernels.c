d016e871badaeb7a538335b747367594ffdda345
liamholland <liamholland02@gmail.com>
2025-01-22 14:51:15 +0000

    pass graph as parameter and prototype kernels

--- src/dynamicKernels.c
+++ src/dynamicKernels.c
@@ -2,10 +2,26 @@
 #include "graphutil.h"
 #include "dynamicKernels.h"
 
-int possiblyRemoveEdgeKernel(node* agent) {
+int possiblyRemoveEdgeKernel(node** graph, node* agent) {
     if(agent->degree > 0 && rand() % 10 == 0) {
         removeEdge(agent, agent->neighbours[rand() % agent->degree]);
     }
+
+    return 0;
+}
+
+int possiblyRemoveNodeKernel(node**, node* agent) {
+    if(rand() % 100 == 0) {
+        //remove the node outright
+    }
+    
+    return 0;
+}
+
+int duplicateTheGraph(node** graph, node* agent) {
+    node** secondGraph = copyGraph(graph, sizeof(graph) / sizeof(node*));
+
+    //connect the graphs together in some way
 
     return 0;
 }
