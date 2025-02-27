56e8bc876deb0d9fb6c4f873e47962ad08014668
liamholland <114741709+liamholland@users.noreply.github.com>
2025-02-27 15:48:52 +0000

    more documentation (#30)
    * print number of local colours in traversal mode
    
    * add graph degree information
    
    * docs

--- include/colouringKernels.h
+++ include/colouringKernels.h
@@ -1,6 +1,14 @@
 #ifndef COLOURERS
 #define COLOURERS
 
+/*
+colouring kernels will change the colour of a node, and move the agent to
+a different node. noteably, these kernels are the only place in the program
+where a node** is not an array of nodes, but a reference to a single node pointer.
+this is necessary in order to allow the agent to "move" (i.e. point to a
+different node)
+*/
+
 #include "node.h"
 
 //KNOWLEDGE:    the agent (fish) only knows if it is conflict, but nothing about the conflict
@@ -29,6 +37,10 @@ int randomKernel(node** agentPointer, int numMoves, int maxColour);
 //MOVES:        moves the agent to a random neighbour
 int edgeChopperKernel(node** agentPointer, int numMoves, int maxColour);
 
+//KNOWLEDGE:    knows what colour is on the node, and the colours of its neighbours
+//COLOURING:    the same as minimum local colour if it is a normal node, if it is an
+//  imposter, it will colour the node with the colour that will cause the greatest number of conflicts
+//MOVES:        same as minimum local colour, but this is untested behaviour and will likely not work
 int amongUsKernel(node** agentPointer, int numMoves, int maxColour);
 
 #endif
