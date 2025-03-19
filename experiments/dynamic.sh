b5c5fb2250a458b0fd01b55fbe0c5b781a46d90f
liamholland <liamholland02@gmail.com>
2025-03-19 14:17:18 +0000

    remove bad dynamic experiments

--- experiments/dynamic.sh
+++ experiments/dynamic.sh
@@ -3,26 +3,26 @@ NUMBER_OF_AUTO_RUNS=10
 MAXIMUM_ITERATIONS=50000
 
 # demonstrate robustness
-for p in 0.2 0.5 0.8 
-do
-    ./colouring -n $NUMBER_OF_NODES -p $p -A $NUMBER_OF_AUTO_RUNS -S res/dyn_n_ -M $MAXIMUM_ITERATIONS -k m -d n
-done
+# for p in 0.2 0.5 0.8 
+# do
+#     ./colouring -n $NUMBER_OF_NODES -p $p -A $NUMBER_OF_AUTO_RUNS -S res/dyn_n_ -M $MAXIMUM_ITERATIONS -k m -d n
+# done
 
-for p in 0.2 0.5 0.8 
-do
-    ./colouring -n $NUMBER_OF_NODES -p $p -A $NUMBER_OF_AUTO_RUNS -S res/dyn_e_ -M $MAXIMUM_ITERATIONS -k m -d e
-done
+# for p in 0.2 0.5 0.8 
+# do
+#     ./colouring -n $NUMBER_OF_NODES -p $p -A $NUMBER_OF_AUTO_RUNS -S res/dyn_e_ -M $MAXIMUM_ITERATIONS -k m -d e
+# done
 
 # identify poor constraints with threshold
 # run this a couple of times with different threshold values
 for p in 0.2 0.5 0.8 
 do
-    ./colouring -n $NUMBER_OF_NODES -p $p -A $NUMBER_OF_AUTO_RUNS -S res/dyn_t_ -M $MAXIMUM_ITERATIONS -k m -d t
+    ./colouring -n $NUMBER_OF_NODES -p $p -A $NUMBER_OF_AUTO_RUNS -S res/dyn_t_ -M $MAXIMUM_ITERATIONS -k m -d t -c 20 -C 20
 done
 
 
 # reduce number of active agents in the graph
 for p in 0.2 0.5 0.8 
 do
-    ./colouring -n $NUMBER_OF_NODES -p $p -A $NUMBER_OF_AUTO_RUNS -S res/dyn_a_ -M $MAXIMUM_ITERATIONS -k m -d a
+    ./colouring -n $NUMBER_OF_NODES -p $p -A $NUMBER_OF_AUTO_RUNS -S res/dyn_a_ -M $MAXIMUM_ITERATIONS -k m -d a -c 20 -C 20
 done
